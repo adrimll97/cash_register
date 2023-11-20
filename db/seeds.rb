ca4f35771143f07c04f36f0c7eccdf7dcf6f04ef
adrimll97 <adrigalileo@gmail.com>
2023-11-20 13:25:54 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Products creation
+Product.create(code: 'GR1', name: 'Green Tea', price: 3.11)
+Product.create(code: 'SR1', name: 'Strawberries', price: 5.0)
+Product.create(code: 'CF1', name: 'Coffee', price: 11.23)
+
+# Promotions
+FreePromotion.create(bougths_products: 1, free_products: 1)
+PricePromotion.create(min_product_quantity: 3, product_discount: '0.9')
+PricePromotion.create(min_product_quantity: 3, product_discount: '2/3')
+
+# ProductPromotions
+ProductPromotion.create(product: Product.first, promotionable: FreePromotion.first)
+ProductPromotion.create(product: Product.second, promotionable: PricePromotion.first)
+ProductPromotion.create(product: Product.last, promotionable: PricePromotion.last)
